764a2cb206841f00b64d169c85ea9647cbd0db82
Mark Brough <mark.brough@gmail.com>
2019-12-04 19:43:00 +0000

    Remove unused imports

--- sageiaticreator/views/activities.py
+++ sageiaticreator/views/activities.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, flash, request, Markup, \
-    session, redirect, url_for, escape, Response, abort, send_file, jsonify, Blueprint
+from flask import render_template, request, jsonify, Blueprint
 from flask_login import login_required, current_user
 
 from sageiaticreator import models
