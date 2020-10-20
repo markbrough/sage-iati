ccc5bfba533078a0fc850fea324b644fe80352da
Mark Brough <mark.brough@gmail.com>
2020-10-20 15:36:24 +0200

    Remove unused routes

--- sageiaticreator/routes.py
+++ sageiaticreator/routes.py
@@ -1,10 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import current_user
 
-from sageiaticreator.extensions import db
-from sageiaticreator import models
 from sageiaticreator import setup as sisetup
-from sageiaticreator.views import users, organisations, activities, generate
 from sageiaticreator.query import organisation as siorganisation
 
 
